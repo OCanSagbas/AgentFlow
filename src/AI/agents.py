81c45d5a17f297b3f6efe2ef6675041774f85195
Ömer Can Sağbaş <o.can.sagbas@gmail.com>
2026-02-07 00:33:12 +0300

    movie discovery agent is added.

--- src/AI/agents.py
+++ src/AI/agents.py
@@ -1,6 +1,6 @@
 from langchain.agents import create_agent
 from AI.llms import get_gemini_model
-from AI.tools import (document_tools)
+from AI.tools import (document_tools, movie_discovery_tools)
 
 
 def get_document_agent(model=None, checkpointer=None):
@@ -12,5 +12,19 @@ def get_document_agent(model=None, checkpointer=None):
     tools=document_tools,
     system_prompt="You are a helpful assistant. You have access to document management tools. Use these tools when users ask about their documents, but feel free to answer other questions using your general knowledge.",
     checkpointer=checkpointer,
+    name="document-assistant"
+    )
+    return agent
+
+def get_movie_discovery_agent(model=None, checkpointer=None):
+
+    llm_model = get_gemini_model(model=model) 
+
+    agent = create_agent(
+        model=llm_model,  
+        tools=movie_discovery_tools,  
+        system_prompt="You are a helpful assistant in finding and discovering information about movies.",
+        checkpointer=checkpointer,
+        name="movie-discovery-assistant"
     )
     return agent
